15aaafb339ac3457453c3c22a53115d9484375a2
Sefagor <168638532+Sefagor@users.noreply.github.com>
2024-11-28 11:23:02 +0100

    Delete assets directory

--- assets/js/script.js
+++
@@ -1,5 +0,0 @@
-function getViewportWidth() {
-    return window.innerWidth || document.documentElement.clientWidth;
-   }
-
-   console.log(`Die Viewport-Breite beträgt: ${getViewportWidth()} Pixel.`);
